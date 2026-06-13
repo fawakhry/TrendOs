0899f36983b85119dc2d51e6e2af4bd89aa04cdc
fawakhry <87904105+fawakhry@users.noreply.github.com>
2026-06-13 17:53:28 +0300

    Update apiUrl in config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.TRENDOS_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbzyKRjlXjg-rniOPX3gSFedtpcT9sXckukqTrmnKh8EyH7GyTmG7kEEx-Qpi28qMHsw/exec"
 };
-  apiUrl: "PUT_APPS_SCRIPT_WEB_APP_URL_HERE",
   defaultScreens: {
     admin: "admin",
     print: "print",
