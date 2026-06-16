1593a876467cb68084b66fd1a2867132b8a6fbc3
fawakhry <87904105+fawakhry@users.noreply.github.com>
2026-06-16 23:51:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,16 @@
 window.TREND_API_URL = "https://script.google.com/macros/s/AKfycbwGHOduL0BHvH-o4up9nbk1wYFi54D2KOnW1AFDigpBzyuAOTWzPfpSFPGSyFVj_fmTmg/exec";
+
+window.MB_CONFIG = {
+  businessName: "مطبعجي بنها",
+  whatsappNumber: "201036112077",
+
+  // نفس رابط TrendOS / Google Apps Script
+  activationEndpoint: window.TREND_API_URL,
+
+  sheetWidthCm: 29.7,
+  sheetHeightCm: 45,
+  dpi: 300,
+  gapMm: 1,
+  outerMarginMm: 2,
+  defaultFitMode: "smart"
+};
