4207fe782f14232e256c2060be5eecb4ca9d8f53
fawakhry <87904105+fawakhry@users.noreply.github.com>
2026-06-22 01:20:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ window.TREND_API_URL = "https://script.google.com/macros/s/AKfycbwGHOduL0BHvH-o4
 
 // V1856 Patch 06 - ملفات مطبعجي من شاشة الموظف بدون تسجيل دخول منفصل.
 // بعد تشغيل Cloudflare Tunnel استخدم: https://files.matbagy.com
-window.MATBAGY_REMOTE_FILES_URL = "https://files.matbagy.com";
-
+window.MATBAGY_REMOTE_FILES_URL = "http://127.0.0.1:5050";
 // للتجربة على نفس جهاز السيرفر قبل تفعيل Cloudflare، يمكن مؤقتاً جعل الرابط أعلاه: http://127.0.0.1:5050
 window.MATBAGY_FILE_SERVER_URL = "http://127.0.0.1:5050";
 
